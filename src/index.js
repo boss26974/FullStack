3e9cedc81772ba0f11e8d41a3a14771b5916f740
boss26974 <pitchaya232@gmail.com>
2022-03-15 12:56:19 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
